5010925b817713153ae410d08ff176b04162853f
MisterMikeDev <diegoerr04@gmail.com>
2023-02-10 12:24:18 -0600

    :tada: Initial Commit.

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+	"editor.formatOnSave": true,
+	"trailingComma": "none",
+	"spaceWidth": 4,
+	"singleQuote": false,
+	"printWidth": 80,
+	"proseWrap": "always",
+	"tabWidth": 4,
+	"requireConfig": false,
+	"useTabs": false,
+	"bracketSpacing": true,
+	"semi": true
+};
